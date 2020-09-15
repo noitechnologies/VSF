cb49cb18a143b3ce47030bf32f01dd51dfa051d0
Roopal Singh <roopal.singh@noitechnologies.com>
2020-09-15 18:06:33 +0530

    Add static pages with the dummy content for VSF App.

---
+++ src/themes/default/components/theme/blocks/Static/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>
+      Vastrami Fashions
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisl neque, ultricies a pharetra at, auctor non risus. Maecenas facilisis laoreet elit vitae sodales. Suspendisse potenti. Integer pulvinar pharetra viverra. Morbi gravida porta ligula, vitae mattis risus consequat ac. Suspendisse dictum leo vel accumsan commodo. Ut volutpat lacinia purus quis tempor. Sed non mauris sed ligula maximus rhoncus. Quisque vel mi lacinia felis dictum sollicitudin vel at eros. Nam sit amet enim arcu.
+    </p>
+    <p>
+      Aliquam ac consequat neque, in ultrices libero. Cras nec lacus mi. Morbi in elit risus. Sed semper dui metus, tincidunt imperdiet diam lacinia sed. Suspendisse non aliquet nulla, at rhoncus massa. Nulla vulputate convallis tortor porta ornare. Duis dictum ex eget nulla pellentesque sagittis. Etiam ac erat in quam varius eleifend. Cras lacinia ante nibh, eu scelerisque diam placerat et. Suspendisse eget facilisis ligula.
+    </p>
+  </div>
+</template>
---
+++ src/themes/default/components/theme/blocks/Static/Privacy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>
+      Vastrami Fashions
+    </h2>
+    <p>
+      Aliquam ac consequat neque, in ultrices libero. Cras nec lacus mi. Morbi in elit risus. Sed semper dui metus, tincidunt imperdiet diam lacinia sed. Suspendisse non aliquet nulla, at rhoncus massa. Nulla vulputate convallis tortor porta ornare. Duis dictum ex eget nulla pellentesque sagittis. Etiam ac erat in quam varius eleifend. Cras lacinia ante nibh, eu scelerisque diam placerat et. Suspendisse eget facilisis ligula.
+    </p>
+    <p>
+    <ul>
+       <li>In eleifend eros quis bibendum fringilla.</li>
+       <li>Vivamus sed leo lobortis, tempor quam vitae, faucibus dui.</li>
+       <li>Proin interdum dui id magna efficitur, a consectetur tortor consectetur.</li>
+       <li>Vivamus condimentum justo varius molestie molestie.</li>
+       <li>Maecenas porttitor purus in efficitur semper.</li>
+      </ul>
+    </p>
+    <p>
+      Morbi elementum ipsum et mi aliquam, ultrices scelerisque turpis tincidunt. Ut rutrum tellus ut mauris ornare, non lacinia mauris consectetur. Aenean id tellus eros. Fusce fringilla sapien id enim consequat, ac elementum velit mollis. Integer auctor imperdiet orci, a rhoncus nulla hendrerit et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris bibendum metus vitae dui semper, vitae dignissim elit commodo.
+    </p>
+  </div>
+</template>
