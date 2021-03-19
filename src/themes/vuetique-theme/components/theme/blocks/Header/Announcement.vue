399b08d7ecd40fa3f6fe845368f721dbfcc4dab8
Deepak Nigam <deepak.nigam@noitechnologies.com>
2021-03-19 17:41:25 +0530

    Added demo site banner.

--- src/themes/vuetique-theme/components/theme/blocks/Header/Announcement.vue
+++ src/themes/vuetique-theme/components/theme/blocks/Header/Announcement.vue
@@ -1,7 +1,10 @@
 <template>
+  <!--
   <div v-if="!isCheckoutPage" class="hidden lg:flex w-full justify-center items-center py-2 bg-grey-lighter">
     <span class="uppercase text-xs tracking-md text-grey-dark font-medium">{{ announcement }}</span>
   </div>
+  -->
+  <marquee style="width: 100%;background-color: #777171;font-family:Cursive;font-size:20pt;color:#f23f00;height:29px;">This is a demo site and no orders will be fulfilled from this store.</marquee>
 </template>
 
 <script>
