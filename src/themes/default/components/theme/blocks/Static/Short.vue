88545d9f7599dcfd7b47413d44532c5c7b97cbc5
Harshit <harshit.mathur@noitechnologies.com>
2021-03-24 09:54:49 +0530

    [SLDT-460]Added the demo content of static pages on all three themes.

--- src/themes/default/components/theme/blocks/Static/Short.vue
+++ src/themes/default/components/theme/blocks/Static/Short.vue
@@ -4,6 +4,12 @@
       This website ("website") is operated by Luma Inc., which includes Luma stores, and Luma Private Sales. This privacy policy only covers information collected at this website, and does not cover any information collected offline by Luma. All Luma websites are covered by this privacy policy.
     </p>
     <h2>
+      Sportires Security
+    </h2>
+    <p>
+      Personal information provided on the website and online credit card transactions are transmitted through a secure server. We are committed to handling your personal information with high standards of information security. We take appropriate physical, electronic, and administrative steps to maintain the security and accuracy of personally identifiable information we collect, including limiting the number of people who have physical access to our database servers, as well as employing electronic security systems and password protections that guard against unauthorized access.
+    </p>
+    <h2>
       Sportires Privacy Policy
     </h2>
     <p>
